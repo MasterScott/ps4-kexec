d8c3168880b1f72bf11ef5347fcee0f563cda7b3
C0rpVultra <45185845+C0rpVultra@users.noreply.github.com>
2018-11-20 23:09:28 +0000

    4.74 Defines (#19)
    * Added 4.74 Defines

--- magic.h
+++ magic.h
@@ -124,6 +124,32 @@
 #define kern_off_dmpml4i 0x21BCC2C
 #define kern_off_dmpdpi 0x21BCC30
 
+#elif defined PS4_4_74
+
+#define kern_off_printf                     0x17F30
+#define kern_off_copyin                     0x149f20
+#define kern_off_copyout                    0x149E40
+#define kern_off_copyinstr                  0x14A5C0
+#define kern_off_kmem_alloc_contig          0x2527A0
+#define kern_off_kmem_free                  0x16E100
+#define kern_off_pmap_extract               0x41EA30
+#define kern_off_pmap_protect               0x421180
+#define kern_off_sched_pin                  0x072D50
+#define kern_off_sched_unpin                0X072D60
+#define kern_off_smp_rendezvous             0x0B2210
+#define kern_off_smp_no_rendevous_barrier   0x0B1FD0
+#define kern_off_icc_query_nowait           0x07FF00
+
+#define kern_off_kernel_map                 0x1B39218
+#define kern_off_sysent                     0x1034790
+#define kern_off_kernel_pmap_store          0x21C5A38
+#define kern_off_Starsha_UcodeInfo 0
+#define kern_off_gpu_devid_is_9924          0x4976A0
+#define kern_off_gc_get_fw_info             0x4A2250
+#define kern_off_pml4pml4i                  0x21C5A28
+#define kern_off_dmpml4i                    0x21C5A2C
+#define kern_off_dmpdpi                     0x21C5E30
+
 #elif defined PS4_5_01
 
 #define kern_off_printf 0x00435C70
